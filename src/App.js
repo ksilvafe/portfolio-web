af5e9009572d7f1e4637349a60e7243ddd901eb1
ksilvafe <ferreirakevin325@gmail.com>
2022-05-02 02:23:22 -0300

    docs: ajusts appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './App.css';
+import { Contatos } from './components/contatos';
+import { Depoimentos } from './components/depoimentos';
+import { Experiencia } from './components/experiencia';
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+import { NavBar } from './components/nav';
+import { Portfolio } from './components/portfolio';
+import { Service } from './components/services';
+import { Sobre } from './components/sobre';
 
 function App() {
   return (
-    <div className="App"></div>
+    <>
+      <Header />
+      <NavBar />
+      <Sobre />
+      <Experiencia />
+      <Service />
+      <Portfolio />
+      <Depoimentos />
+      <Contatos />
+      <Footer /> 
+    </>
   );
 }
 
